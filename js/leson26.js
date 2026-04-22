61be8337f4a6fbc55977cb11d830453acd421c6f
Vktoriia <natasukhanova18@gmail.com>
2026-04-22 13:20:30 +0200

    сделано задание 26

---
+++ js/leson26.js
@@ -0,0 +1,31 @@
+
+// Створити телефонну книгу
+// створити початковий клас Abonent, де зберігатимуться ім*я і номер створити set який прийматиме телефон і номер
+// створити get який виводитиме данні про абонента
+// створити три різних юзери
+// вивести данні
+
+
+
+// class Abonent {
+//     constructor(options) {
+//         this.name = options.name;
+//         this.number = options.number;
+//     }
+
+//     set number(value) {
+//         this._number = value;
+//     }
+//     get info() {
+//         return `${this.name}:${this._number}`
+        
+//     }
+// }
+
+// const user1 = new Abonent({ name: "Vika", number: "1998"});
+// const user2 = new Abonent ({ name: "Ivan", number: "1388"});
+// const user3 = new Abonent({ name: "Vasya", number: "2005"});
+  
+// console.log(user1.info);
+// console.log(user2.info);
+// console.log(user3.info);
